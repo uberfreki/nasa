f03268c928d6caf7087126a22e27fd8abf6c4946
Rob Woodall <rob@creative-standard.com>
2015-04-11 11:52:37 -0500

    started on app ui

--- app/www/js/index.js
+++ app/www/js/index.js
@@ -17,12 +17,14 @@ var app = {
             document.addEventListener('deviceready', this.deviceReady, false);
             document.addEventListener('pause', this.pause, false);
             document.addEventListener('resume', this.resume, false);
+            app.isMobile = true;
         } else {
             document.addEventListener('DOMContentLoaded', this.browserReady, false);
+            app.isMobile = false;
         }
     },
     deviceReady: function() {
-        $('#take').click(function(){
+        $('#camera').click(function(){
             app.takePhoto();
         });
         $('#choose').click(function(){
@@ -30,37 +32,46 @@ var app = {
         });
         FastClick.attach(document.body);
     },
+    browserReady: function() {
+        console.log('browser ready');
+        $('#camera').click(function(){
+            $('#take').hide();
+            $('#input').show();
+            $('#fake').show();
+        });
+    },
     pause: function() {
         console.log('app paused');
     },
     resume: function() {
         console.log('app resumed');
     },
-    browserReady: function() {
-        console.log('browser ready');
-        $('#take').click(function(){
-            alert('take');
-        });
-    },
     takePhoto: function() {
-        navigator.camera.getPicture(app.addPhoto, app.errorHandler, { 
-            quality: 40,
-            destinationType: Camera.DestinationType.DATA_URL,
-            saveToPhotoAlbum: true,
-            correctOrientation: false,
-            targetWidth: 1024,
-            targetHeight: 768
-        });
+            $('#take').hide();
+            $('#input').show();
+            navigator.camera.getPicture(app.addPhoto, app.errorHandler, { 
+                quality: 40,
+                destinationType: Camera.DestinationType.DATA_URL,
+                saveToPhotoAlbum: true,
+                correctOrientation: false,
+                targetWidth: 1024,
+                targetHeight: 768
+            });
     },
     addPhoto: function(imageData) {
         var photo = $('<img/>').appendTo('#photos');
         $(photo).attr('src','data:image/jpeg;base64,' + imageData);
+        $('#photo').empty().append(photo);
         $(photo).click(function(e){
             $(this).remove();
         });
     },
-    removePhoto: function(e) {
-
+    goBack: function() {
+        $('#take').show();
+        $('#input').hide();
+    },
+    uploadPhoto: function(e) {
+        alert('upload');
     },
     choosePhoto: function() {
         navigator.camera.getPicture(app.addPhoto, app.errorHandler, { 
